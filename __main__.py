f5a5d7f4f1d00e8a956825122487e1dca0bea809
opethef10 <onur.arikan_01@metu.edu.tr>
2021-04-03 14:32:01 +0300

    initial commit hehe
    some descr

---
+++ __main__.py
@@ -0,0 +1,20 @@
+#! python -i -B
+import json
+from pathlib import Path
+from datetime import date
+from utilities import dateParser, dayofyear, fixedCal, march21, doğumGünleri, onBin
+
+
+
+BUGÜN = date.today()
+JSON_PATH = Path(__file__).parent / "birthdays.json"
+with JSON_PATH.open(encoding="utf-8") as jsonFile:
+    birthdays = json.load(jsonFile, object_hook=dateParser)
+
+print(f"Today: {BUGÜN:%d/%m/%Y %A} - Day {dayofyear(BUGÜN)} of year")
+print(f"{BUGÜN.year} Doomsday: {date(BUGÜN.year,10,31):%A}")
+print(f"Fixed Calendar: {fixedCal(BUGÜN)}")
+print(f"March 21 Calendar: {march21(BUGÜN)}")
+print()
+print(doğumGünleri(birthdays))
+print(onBin(birthdays))
