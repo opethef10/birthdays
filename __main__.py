5c2640e133e15150261d10bab9c5bb6b486f80b2
opethef10 <onur.arikan_01@metu.edu.tr>
2021-08-25 00:06:50 +0300

    Updated and improved functions

--- __main__.py
+++ __main__.py
@@ -1,20 +1,21 @@
-#! python -i -B
 import json
 from pathlib import Path
 from datetime import date
-from utilities import dateParser, dayofyear, fixedCal, march21, doğumGünleri, onBin
-
-#comment again
+from utilities import dateParser, dayOfYear, fixedCal, march21, doğumGünleri, onBin
 
 BUGÜN = date.today()
 JSON_PATH = Path(__file__).parent / "birthdays.json"
 with JSON_PATH.open(encoding="utf-8") as jsonFile:
-    birthdays = json.load(jsonFile, object_hook=dateParser)
+    birthdays = json.load(jsonFile, object_hook = dateParser)
 
-print(f"Today: {BUGÜN:%d/%m/%Y %A} - Day {dayofyear(BUGÜN)} of year")
+print(f"Today: {BUGÜN:%d/%m/%Y %A} - Day {dayOfYear(BUGÜN)} of year")
 print(f"{BUGÜN.year} Doomsday: {date(BUGÜN.year,10,31):%A}")
 print(f"Fixed Calendar: {fixedCal(BUGÜN)}")
 print(f"March 21 Calendar: {march21(BUGÜN)}")
 print()
-print(doğumGünleri(birthdays))
-print(onBin(birthdays))
+for kişi, doğumGünü in birthdays.items():
+    print(doğumGünleri(kişi, doğumGünü, BUGÜN))
+print() 
+print("10000 Days:") 
+for kişi, doğumGünü in birthdays.items():
+    print(onBin(kişi, doğumGünü, BUGÜN))
--- utilities.py
+++ utilities.py
@@ -1,70 +1,66 @@
 from datetime import date, timedelta
-BUGÜN = date.today()
 
-def doğumGünleri(dic):
-    result = ""
-    for kişi, doğumGünü in dic.items():
-        günler = (BUGÜN-doğumGünü).days
-        yaş = age(doğumGünü)
-        result += f"{kişi:<10} {doğumGünü:%d/%m/%Y %a} {günler:>6} days {yaş:>6.2f} years\n"
-    return result
+march21Margins = 31, 30, 31, 31, 31, 31, 30, 30, 30, 30, 30, 30
+fixedMonths = (
+    "Jan", "Feb", "Mar", "Apr", "May", "Jun", 
+    "Sol", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+)
 
-def onBin(dic,günler=10000):
-    result = f"{günler} Days:\n"
-    for kişi, doğumGünü in dic.items():
-        onBin = doğumGünü + timedelta(günler)
-        result += f"{kişi:<10} {onBin:%d %b %Y} in {(onBin-BUGÜN).days:>6} days\n"
-    return result
-    
+def doğumGünleri(kişi, doğumGünü, today):
+    günler = (today - doğumGünü).days
+    yaş = age(doğumGünü, today)
+    return f"{kişi:<10} {doğumGünü:%d/%m/%Y %a} {günler:>6} days {yaş:>6.2f} years"
+
+def onBin(kişi, doğumGünü, today, günler = 10000):
+    onBin = doğumGünü + timedelta(günler)
+    return f"{kişi:<10} {onBin:%d %b %Y} in {(onBin-today).days:>6} days"
+
 def dateParser(dct):
     for k, v in dct.items():
         dct[k] = date.fromisoformat(v)
     return dct
+    
+###########################
+    
+def age(birth, today):
+    bday = birth.replace(year = today.year) 
+    return today.year - birth.year + ((today - bday) / timedelta(365))
 
-def dayofyear(tarih):
+def dayOfYear(tarih):
     return (tarih - date(tarih.year, 1, 1) + timedelta(1)).days
 
-def _dayofyear2(tarih):
+def dayOfYear21March(tarih):
     equinox = date(tarih.year, 3, 21)
     if tarih < equinox:
         equinox = date(tarih.year - 1, 3, 21)
     return (tarih - equinox + timedelta(1)).days
 
 def fixedCal(tarih):
-    aylar = ("Jan", "Feb", "Mar", "Apr", "May", "Jun", 
-             "Sol", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
     if tarih.month==2 and tarih.day==29:
         return "29 Sol"
+    
     if tarih.month==12 and tarih.day==31:
         return "29 Dec"
     
     yıl = tarih.year
-    ay, gün = divmod(dayofyear(tarih), 28)
+    ay, gün = divmod(dayOfYear(tarih), 28)
     if yıl%4==0 and tarih >= date(yıl, 3, 1):
         gün -= 1
+    
     if gün <= 0:
         ay -= 1
         gün += 28
-    return f"{gün} {aylar[ay]} {yıl}"
-
-def _fixedCal2(tarih):
-    aylar = "ABCDEFGHIJKLM"
-    ay, gün = divmod(_dayofyear2(tarih), 28)
-    return f"{gün}/{ay+1}{aylar[ay]}"
+    
+    return f"{gün} {fixedMonths[ay]} {yıl}"
 
 def march21(tarih):
-    aralıklar = 31, 30, 31, 31, 31, 31, 30, 30, 30, 30, 30, 30
-    gün = _dayofyear2(tarih)
-    yıl = tarih.year
-    if tarih < date(yıl, 3, 21):
-        yıl-=1
+    d = dayOfYear21March(tarih)
+    y = tarih.year
+    if tarih < date(y, 3, 21):
+        y -= 1
     
-    for ay, aralık in enumerate(aralıklar, 1):
-        if gün > aralık:
-            gün -= aralık
+    for m, margin in enumerate(march21Margins, 1):
+        if d > margin:
+            d -= margin
         else:
-            return f"{gün:02}/{ay:02}/{yıl}"
-
-def age(birth):
-    bday = birth.replace(year = BUGÜN.year) 
-    return BUGÜN.year - birth.year + ((BUGÜN - bday) / timedelta(365))
+            return f"{d:02}/{m:02}/{y}"
