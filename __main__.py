f16afa94070a874aee57cf17275d8f9cf8126233
Onur Arıkan <onur93arikan@gmail.com>
2024-10-12 16:31:25 +0300

    Add file parameter for command line

--- __main__.py
+++ __main__.py
@@ -13,18 +13,22 @@ from pathlib import Path
 from utilities import dateParser, dayOfYear, fixedCal, march21, doğumGünleri, onBin
 
 TODAY = date.today()
-JSON_PATH = Path(__file__).parent / "birthdays.json"
+MAIN_DIRECTORY = Path(__file__).parent
+DEFAULT_JSON_FILE_NAME = "birthdays.json"
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Display birthday information of the people')
     parser.add_argument('-t', '--tenThousand', action='store_true', help="Display 10000th day data of the players")
+    parser.add_argument('-f', '--file', type=str, help="Path to the JSON file with birthday data")
     return parser.parse_args()
 
 
 if __name__ == "__main__":
     args = parse_arguments()
 
-    with JSON_PATH.open(encoding="utf-8") as jsonFile:
+    # Use the specified file if provided, otherwise fallback to the default path
+    json_path = MAIN_DIRECTORY / (args.file if args.file else DEFAULT_JSON_FILE_NAME)
+    with json_path.open(encoding="utf-8") as jsonFile:
         birthdays = json.load(jsonFile, object_hook = dateParser)
 
     print(f"Today: {TODAY:%d/%m/%Y %A} - Day {dayOfYear(TODAY)} of year")
