7bbf32497b62734a4254c45945a9ad3f34a70a21
Onur Arıkan <onur93arikan@gmail.com>
2024-12-19 03:42:44 +0300

    Refactor

--- __main__.py
+++ __main__.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python3
 """
-This program takes a JSON input of name-birthdate pairs and gives information about how many days, week and years old. Additionally it gives an information about how many days these people are close to their 10000th day in their lives.
+This program takes a JSON input of name-birthdate pairs and gives information
+about how many days, week and years old. Additionally it gives an information
+about how many days these people are close to their 10000th day in their lives.
 """
 
 # Standard library imports
@@ -16,30 +18,46 @@ TODAY = date.today()
 MAIN_DIRECTORY = Path(__file__).parent
 DEFAULT_JSON_FILE_NAME = "birthdays.json"
 
+
 def parse_arguments():
-    parser = argparse.ArgumentParser(description='Display birthday information of the people')
-    parser.add_argument('-t', '--tenThousand', action='store_true', help="Display 10000th day data of the players")
-    parser.add_argument('-f', '--file', type=str, help="Path to the JSON file with birthday data")
+    parser = argparse.ArgumentParser(
+        description="Display birthday information of the people"
+    )
+    parser.add_argument(
+        "-t",
+        "--tenThousand",
+        action="store_true",
+        help="Display 10000th day data of the players",
+    )
+    parser.add_argument(
+        "-f", "--file", type=str, help="Path of JSON file with birthday data"
+    )
     return parser.parse_args()
 
 
 if __name__ == "__main__":
     args = parse_arguments()
 
-    # Use the specified file if provided, otherwise fallback to the default path
-    json_path = MAIN_DIRECTORY / (args.file if args.file else DEFAULT_JSON_FILE_NAME)
+    # Use the specified file if provided
+    # Otherwise fallback to the default path
+    file_name = args.file if args.file else DEFAULT_JSON_FILE_NAME
+    json_path = MAIN_DIRECTORY / file_name
     if not json_path.exists():
         if args.file:
-            raise SystemExit(f"FileNotFoundError: No such file or directory: '{json_path}'")
+            raise SystemExit(
+                f"FileNotFoundError: No such file or directory: '{json_path}'"
+            )
         raise SystemExit(
             f"FileNotFoundError: No such file or directory: '{json_path}'\n"
-            f"Either add a file named '{DEFAULT_JSON_FILE_NAME}' (preferably copying from '{DEFAULT_JSON_FILE_NAME}.example') or enter a valid path using the -f command line option."
+            f"Either add a file named '{DEFAULT_JSON_FILE_NAME}'"
+            f"(preferably copying from '{DEFAULT_JSON_FILE_NAME}.example')"
+            f"or enter a valid path using the -f command line option."
         )
     with json_path.open(encoding="utf-8") as jsonFile:
-        birthdays = json.load(jsonFile, object_hook = dateParser)
+        birthdays = json.load(jsonFile, object_hook=dateParser)
 
     print(f"Today: {TODAY:%d/%m/%Y %A} - Day {dayOfYear(TODAY)} of year")
-    print(f"{TODAY.year} Doomsday: {date(TODAY.year,10,31):%A}")
+    print(f"{TODAY.year} Doomsday: {date(TODAY.year, 10, 31):%A}")
     print(f"Fixed Calendar: {fixedCal(TODAY)}")
     print(f"March 21 Calendar: {march21(TODAY)}")
     print()
@@ -51,4 +69,3 @@ if __name__ == "__main__":
         print("10000 Days:")
         for person, birthdate in birthdays.items():
             print(onBin(person, birthdate, TODAY))
-
--- utilities.py
+++ utilities.py
@@ -1,11 +1,24 @@
-"""Some utility functions that are useful to make date and calendar calculations"""
+"""Utility functions that are useful to make date and calendar calculations"""
+
 from datetime import date, timedelta
 
 MARCH_21_MARGINS = 31, 30, 31, 31, 31, 31, 30, 30, 30, 30, 30, 30
 FIXED_MONTHS = (
-    "Jan", "Feb", "Mar", "Apr", "May", "Jun", 
-    "Sol", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Sol",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
 )
+
 
 def doğumGünleri(name: str, birthdate: date, today: date) -> str:
     days: int = (today - birthdate).days
@@ -13,25 +26,28 @@ def doğumGünleri(name: str, birthdate: date, today: date) -> str:
     current_age: float = _age(birthdate, today)
     return f"{name:<18} {birthdate:%d/%m/%Y %a} {days:>6} days {weeks:>5} weeks {current_age:>6.2f} years"
 
+
 def onBin(name: str, birthdate: date, today: date, milestone: int = 10000) -> str:
     tenThousandthDay: date = birthdate + timedelta(milestone)
-    daysUntilTenThousandth: timedelta = tenThousandthDay-today
+    daysUntilTenThousandth: timedelta = tenThousandthDay - today
     return f"{name:<10} {tenThousandthDay:%d %b %Y} in {daysUntilTenThousandth.days:>6} days"
+
 
 def dateParser(json_data: dict[str, str]) -> dict[str, date]:
     return {k: date.fromisoformat(v) for k, v in json_data.items()}
-    
-###########################
-    
+
+
 def _age(birthdate: date, today: date) -> float:
     """Calculate the age of someone with floating point precision"""
-    birthday: date = birthdate.replace(year = today.year)
+    birthday: date = birthdate.replace(year=today.year)
     return today.year - birthdate.year + ((today - birthday) / timedelta(days=365))
+
 
 def dayOfYear(today: date) -> int:
     """Return which day of the year today's date is"""
     firstDayOfYear: date = date(today.year, 1, 1)
     return (today - firstDayOfYear + timedelta(1)).days
+
 
 def _dayOfYear21March(today: date) -> int:
     """Return which day of the year today's date is relative to March 21"""
@@ -40,28 +56,30 @@ def _dayOfYear21March(today: date) -> int:
         equinox = equinox.replace(year=today.year - 1)
     return (today - equinox + timedelta(days=1)).days
 
+
 def fixedCal(today: date) -> str:
     """Get International Fixed Calendar representation of the given date"""
     year: int = today.year
-    if today.month==2 and today.day==29:
+    if today.month == 2 and today.day == 29:
         return f"29 Sol {year}"
-    
-    if today.month==12 and today.day==31:
+
+    if today.month == 12 and today.day == 31:
         return f"29 Dec {year}"
-    
+
     month, day = divmod(dayOfYear(today), 28)
     march1: date = date(year, 3, 1)
-    
+
     if year % 4 == 0 and today >= march1:
         # Leap year edge case
         day -= 1
-    
+
     if day <= 0:
-        # In case the divmod or leap year calculation result makes the day 0 or less
+        # In case the divmod or leap year calculation makes the day 0 or less
         month -= 1
         day += 28
 
     return f"{day} {FIXED_MONTHS[month]} {year}"
+
 
 def march21(today: date) -> str:
     """Get the date representation of the calendar that starts with March 21"""
@@ -70,7 +88,7 @@ def march21(today: date) -> str:
     equinox: date = date(year, 3, 21)
     if today < equinox:
         year -= 1
-    
+
     for month, margin in enumerate(MARCH_21_MARGINS, 1):
         if day > margin:
             day -= margin
@@ -78,4 +96,3 @@ def march21(today: date) -> str:
             return f"{day:02}/{month:02}/{year}"
     # In case of a leap year
     return f"31/12/{year}"
-
